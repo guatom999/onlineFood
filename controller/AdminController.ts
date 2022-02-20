d9aec75e8af6283906055828698fd1fad301247d
guatom999 <tngs159@hotmail.com>
2022-02-21 04:12:03 +0700

    second

--- controller/AdminController.ts
+++ controller/AdminController.ts
@@ -1,6 +1,8 @@
 import { Request, Response , NextFunction } from "express";
 import {CreateVandorInput} from "../dto"
 import { Vandor } from "../models"
+import { GenerateSalt , GeneratePassword } from "../utility";
+
 
 export const CreateVandor = async (req: Request, res: Response, next: NextFunction) => {
    
@@ -12,16 +14,19 @@ export const CreateVandor = async (req: Request, res: Response, next: NextFuncti
         return res.json({"message":"A vandor is exist with this email ID "})
     }
 
+    const salt = await GenerateSalt();
+    const userPassword = await GeneratePassword(passwords, salt);
+
     const createdVandor = Vandor.create({
         name:name,
         address:address,
         pincode:pincode,
         foodType:foodType,
         email:email,
-        passwords:passwords,
+        passwords:userPassword,
         ownerName:ownerName,
         phone:phone,
-        salt:'Bossza',
+        salt:salt,
         rating:0,
         serviceAvailable:false,
         coverImage: [],
@@ -32,6 +37,14 @@ export const CreateVandor = async (req: Request, res: Response, next: NextFuncti
 
 export const GetVandors = async (req: Request, res: Response, next: NextFunction) => {
     
+    const vandors = await Vandor.find();
+
+    if(vandors !== null){
+        return res.json(vandors)
+    }
+    else{
+        return res.json({"message":"vandors data not available"})
+    }
 }
 
 export const GetVandorByID = async (req: Request, res: Response, next: NextFunction) => {
